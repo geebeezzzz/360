2a15bcea9c348dd832fc61838515351b58d179cf
grant <30723719+geebeezzzz@users.noreply.github.com>
2019-05-08 13:20:57 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,29 @@ console.log(window.location);
 
 
 document.getElementById("whatnow").innerHTML = "Page search is " + window.location.search.substring(1);
+
+
+//console.log('api.tonsser.com/50/users/db264873-b9d5-4b4c-8a06-4ae6e6f8fa5c');
+
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://api.tonsser.com/50/users/db264873-b9d5-4b4c-8a06-4ae6e6f8fa5c', true)
+request.onload = function() {
+	
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    console.log(data.response.shield.rating.overall)
+    }
+	
+   else {
+    console.log('error')
+  }
+}
+
+request.send()
+
+
+
+
