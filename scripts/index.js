0b7812e4a524e6623a3601051752d138106bb644
grant <30723719+geebeezzzz@users.noreply.github.com>
2019-05-20 16:27:12 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+// JavaScript Document
+
+console.log("test");
+
+fetch('data.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+
+        function appendData(data) {
+			
+            var mainContainer = document.getElementById("myData");
+			
+			//////////////////////////////////CHANGING////////////////////////
+			myObj = data[0]; 
+			myObj.firstName = "bad";
+			console.log(myObj);
+			////////////////////////////////////////////////////////////////
+			
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+                mainContainer.appendChild(div);
+            }
+			
+			console.log(data[0].firstName);		
+			
+			//https://staging-api.tonsser.com/50/users/david-pedersen
+				
+//			myObj = data[0]; 
+//			myObj.firstName = "bad";
+//			console.log(myObj);		
+		}
+
+
+const app = document.getElementById('root')
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://staging-api.tonsser.com/50/users/david-pedersen', true)
+
+request.onload = function () {
+
+	var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+	  
+	  console.log(data);
+	  
+    
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+	
+	
+	
+  }
+
+// Send request
+request.send()
