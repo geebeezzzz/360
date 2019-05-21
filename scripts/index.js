a56191c8afbd1b19b614092933a93bfad95b0632
grant <30723719+geebeezzzz@users.noreply.github.com>
2019-05-21 16:30:44 +0200

    json interation 01

--- scripts/index.js
+++ scripts/index.js
@@ -26,70 +26,70 @@ var animation = bodymovin.loadAnimation({
 //
 //
 ///////////////////////////////////////////////////////////////////GET URL INFO OUT/////////////////////////////////////////////	
-//
-//
-//
-//function getQueryVariable(variable)
-//{
-//       var query = window.location.search.substring(1);
-//       var vars = query.split("&");
-//       for (var i=0;i<vars.length;i++) {
-//               var pair = vars[i].split("=");
-//               if(pair[0] == variable){return pair[1];}
-//       }
-//       return(false);
-//}
-//
-//console.log(getQueryVariable("slug"));
-//
-//
-////                https://youthful-feynman-7811c2.netlify.com/?slug=david-pedersen
-//
-///////////////////////////////////////////////////////////////////GET URL INFO OUT/////////////////////////////////////////////	
-//
-//
-//
-//// Create a request variable and assign a new XMLHttpRequest object to it.
-//var mrSlug = 'startState'
-//
-//var request = new XMLHttpRequest()
-//
-//// Open a new connection, using the GET request on the URL endpoint
-//request.open('GET', 'https://staging-api.tonsser.com/50/users/' + getQueryVariable("slug"), true) // getting the user slug from the metaData
-//// request.open('GET', 'https://staging-api.tonsser.com/50/users/david-pedersen', true) // original api call
-//
-//request.onload = function () {
-//	
-//	var data = JSON.parse(this.response)
-//	
-//	{
-//	mrSlug = data.response;
-//	}
-//	
-//	console.log(mrSlug);
-//	
-//  if (request.status >= 200 && request.status < 400) {
-///////////////////////////////////////////////////////////////////PLACE TO RUN USING API DATA/////////////////////////////////////////////	  
-//	
-//	  
-//	  console.log(data.response.firstname);
-//	  
-///////////////////////////////////////////////////////////////////PLACE TO RUN USING API DATA/////////////////////////////////////////////	 	  
-//	  
-//	  
-//    
-//  } else {
-//    const errorMessage = document.createElement('marquee')
-//    errorMessage.textContent = `Gah, it's not working!`
-//    app.appendChild(errorMessage)
-//  }
-//	
-//	
-//	
-//  }
-//
-//// Send request
-//request.send()
+
+
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+console.log(getQueryVariable("slug"));
+
+
+//                https://youthful-feynman-7811c2.netlify.com/?slug=david-pedersen
+
+/////////////////////////////////////////////////////////////////GET URL INFO OUT/////////////////////////////////////////////	
+
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var mrSlug = 'startState'
+
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://staging-api.tonsser.com/50/users/' + getQueryVariable("slug"), true) // getting the user slug from the metaData
+// request.open('GET', 'https://staging-api.tonsser.com/50/users/david-pedersen', true) // original api call
+
+request.onload = function () {
+	
+	var data = JSON.parse(this.response)
+	
+	{
+	mrSlug = data.response;
+	}
+	
+	console.log(mrSlug);
+	
+  if (request.status >= 200 && request.status < 400) {
+/////////////////////////////////////////////////////////////////PLACE TO RUN USING API DATA/////////////////////////////////////////////	  
+	
+	  
+	  console.log(data.response.firstname);
+	  
+/////////////////////////////////////////////////////////////////PLACE TO RUN USING API DATA/////////////////////////////////////////////	 	  
+	  
+	  
+    
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+	
+	
+	
+  }
+
+// Send request
+request.send()
 //
 //
 //
@@ -97,25 +97,27 @@ var animation = bodymovin.loadAnimation({
 //
 //
 //function myWaiter() {
-//	fetch('data.json')
-//            .then(function (response) {
-//                return response.json();
-//            })
-//            .then(function (data) {
-//                appendData(data);
-//            })
-//            .catch(function (err) {
-//                console.log('error: ' + err);
-//            });
-//
-//		
-//
-//        function appendData(data) {
-//			
-//            var mainContainer = document.getElementById("myData");
+	fetch('data.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+
+		
+
+        function appendData(data) {
+			
+            var mainContainer = document.getElementById("myData");
 //			
 //			//////////////////////////////////CHANGING////////////////////////
-//			
+			myObj = data;
+			console.log(myObj);
+			
 //			myObj = data[0]; 
 //			myObj.firstName = 'ola';
 //			console.log(myObj);
@@ -140,7 +142,7 @@ var animation = bodymovin.loadAnimation({
 //			//			myObj = data[0]; 
 //			//			myObj.firstName = "bad";
 //			//			console.log(myObj);		
-//		}
+		}
 //	
 //	}
 
