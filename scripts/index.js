b8784637d2803c998a9284a068210441c6098f0c
grant <30723719+geebeezzzz@users.noreply.github.com>
2019-05-21 11:40:02 +0200

    url slug get

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,36 @@ const app = document.getElementById('root')
 
 
 
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////GET URL INFO OUT/////////////////////////////////////////////	
+
+
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+console.log(getQueryVariable("slug"));
+
+
+/////////////////////////////////////////////////////////////////GET URL INFO OUT/////////////////////////////////////////////	
+
+
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var mrSlug = 'startState'
 
